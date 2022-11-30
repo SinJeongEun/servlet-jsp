3478901893fbbf219f1e3658cc1afde86e5c367e
Jeong eun <lisasje25@gmail.com>
2022-11-30 18:00:24 +0900

    쿠키 사용법

--- src/servlet/exam06/CreateCookieController.java
+++ src/servlet/exam06/CreateCookieController.java
@@ -16,14 +16,17 @@ public class CreateCookieController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 쿠키 생성
 		Cookie cookie1 = new Cookie("useremail", "winter@mycompany.com");
-		cookie1.setDomain("localhost"); // 해당 도메인으로 갈때만 쿠키를 전달한다.
-		cookie1.setPath("/"); 			// 해당 결로로 요청할때만 쿠키를 전달한다.
-		cookie1.setMaxAge(10);			// 쿠키의 유지 기잔을 설정하여 브라우저의 파일 시스템에 저장한다
-		cookie1.setHttpOnly(true); 		// java script 코드로 cookie 읽는 코드 무력화 시키기
-		cookie1.setSecure(true); 		// https 에서만 쿠키를 전달한다는 의미
+//		cookie1.setDomain("localhost"); // 해당 도메인으로 갈때만 쿠키를 전달한다.
+//		cookie1.setPath("/"); 			// 해당 결로로 요청할때만 쿠키를 전달한다.
+//		cookie1.setMaxAge(10);			// 쿠키의 유지 기잔을 설정하여 브라우저의 파일 시스템에 저장한다
+//		cookie1.setHttpOnly(true); 		// java script 코드로 cookie 읽는 코드 무력화 시키기
+//		cookie1.setSecure(true); 		// https 에서만 쿠키를 전달한다는 의미
 		
-		// 쿠키를  HTTP 응답 헤더에 추가하기
+		// 쿠키를  HTTP 응답 헤더에 추가하기                                  
 		response.addCookie(cookie1);
+		
+		Cookie cookie2 = new Cookie("userid", "winter");
+		response.addCookie(cookie2);
 		
 		response.sendRedirect("ContentController");
 	}
